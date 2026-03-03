8f431e95d26122431f755b3a4b0c3f33ff3a8daf
cyrus alde <12345678+aldecyrus425@users.noreply.github.com>
2026-03-03 17:52:22 +0800

    First Commit

---
+++ MyApp.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
